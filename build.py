8e73df791ca9a28d67d4ec862fd56518acbcadc9
Hank Lee <ckshoupon@hotmail.com>
2022-07-13 01:13:18 +0800

    second example

--- build.py
+++ build.py
@@ -15,3 +15,15 @@ default_task = "publish"
 @init
 def set_properties(project):
     pass
+# # key-value based configuration
+# @init
+# def set_properties(project):
+#     project.set_property('coverage_break_build', True)
+#     project.set_property('flake8_break_build', True)
+#     project.set_property('coverage_threshold_warn', 75)
+#     project.build_depends_on("mock")
+#     for dependency in ["docopt", "ultrajson", "treq"]:
+#         project.depends_on(dependency)
+#     project.depends_on("twisted", ">=13.2.0")
+#     project.set_property("install_dependencies_index_url",
+#                          "http://devpi-server.invalid/root/pypi")
---
+++ src/main/scripts/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from iqutils.data_preprocess import say_hi
+print("hi")
+
+
+def main_run():
+    return(say_hi())
+
+
+if __name__ == "__main__":
+    main_run()
