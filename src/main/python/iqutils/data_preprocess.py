03e402ccab14e5e302c6bff7f75a702954731d31
Hank Lee <ckshoupon@hotmail.com>
2022-07-12 22:59:48 +0800

    init commit

---
+++ src/main/python/iqutils/data_preprocess.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+
+def say_hi():
+    print("Hi, there!")
+# class ImgProcessor:
+#     def __init__(self, width, height):
+#         self.width = width
+#         self.height = height
+#     def resize(self, img):
+#         if (img.shape[0] != self.width) or (img.shape[1] != self.height):
+#             new_img = img.resize((self.width, self.height))
